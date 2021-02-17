4fdf618cdde7b2436f7eef8a0f7d78a63f83da62
unknown <igr-kndratv@ukr.net>
2021-02-17 19:05:25 +0200

    'test1.1'

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
+import WithRestoService from '../hoc';
+import {Switch, Route} from 'react-router-dom';
 
 import Background from './food-bg.jpg';
 
-const App = () => {
+const App = ({RestoService}) => {
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
             <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+
+            <Switch>    
+                <Route path="/" exact component={MainPage}/>
+                <Route path="/card" component={CartPage}/>
+                <Route exact component={MainPage}/>
+            </Switch>
         </div>
     )
 }
 
-export default App;
+export default WithRestoService() (App);
